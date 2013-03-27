361deb922d1f20577ab514d376c0a9654f0c677c
Mitch Malone <mitch.malone@me.com>
2013-03-27 13:22:38 +1100

    GA

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -1,11 +1,14 @@
 $script([
 	'js/jquery.min.js',
-	'js/twitter.bootstrap.min.js'
+	'js/twitter.bootstrap.js'
 ], function() {
 	$script([
 		'js/retina-test.js'
 	], function() {
 		$(document).ready(function(){
+			// No console logging
+			console.log = $.noop;
+			
 			RETINA.test.init();
 		});
 	});
